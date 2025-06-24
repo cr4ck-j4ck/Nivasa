b760664aef12997570b0148d7e3a5bf1ff6fc28f
cr4ck-j4ck <vermapratyush486@gmail.com>
2025-06-24 18:30:19 +0530

    fix(init.js): correct model variable naming and restructure data initialization

--- Backend/init.js
+++ Backend/init.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const connectDB = require("./config/db");
-const ListingModel = require("./Models/ListingModel");
+const listingModel = require("./Models/ListingModel");
+const usersModel = require("./Models/UsersModel");
 
 async function initData() {
 
@@ -9,9 +10,12 @@ async function initData() {
     await connectDB();
     console.log('MongoDB connected.');
 
-    const data = JSON.parse(fs.readFileSync('./SampleData/listingsData.json', 'utf8'));
-    await ListingModel.deleteMany({});
-    await ListingModel.insertMany(data);
+    const data1 = JSON.parse(fs.readFileSync('./SampleData/userData.json', 'utf8'));
+    const data2 = JSON.parse(fs.readFileSync('./SampleData/listingsData.json', 'utf8'));
+    await usersModel.deleteMany({});
+    await usersModel.insertMany(data1);
+    await listingModel.deleteMany({});
+    await listingModel.insertMany(data2);
   } catch (err) {
     console.error('Error:', err);
   }
