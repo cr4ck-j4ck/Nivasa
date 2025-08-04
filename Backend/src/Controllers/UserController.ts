895d0b24793c6650cbfc3560cc35c69ef822aafd
cr4ck-j4ck <vermapratyush486@gmail.com>
2025-08-04 18:45:23 +0530

    Changed Same Site to none for production

--- Backend/src/Controllers/UserController.ts
+++ Backend/src/Controllers/UserController.ts
@@ -61,13 +61,13 @@ export const loginUser: RequestHandler = async (req, res) => {
         res.cookie("token", token, {
           httpOnly: true,
           secure: process.env.NODE_ENV === "production",
-          sameSite: "strict",
+          sameSite:"none",
           maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         });
         res.cookie("refreshToken", refreshToken, {
           httpOnly: true,
           secure: process.env.NODE_ENV === "production",
-          sameSite: "strict",
+          sameSite: "none",
           maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
         });
         const { password, ...responseObject } = existingUser;
