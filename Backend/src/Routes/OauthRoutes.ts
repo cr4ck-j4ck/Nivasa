da6971f5aa88b0a149fc4e05d22d6b747f7132a4
cr4ck-j4ck <vermapratyush486@gmail.com>
2025-08-05 09:01:37 +0530

    Changed strict true to none because production website is on difference domain

--- Backend/src/Routes/OauthRoutes.ts
+++ Backend/src/Routes/OauthRoutes.ts
@@ -16,14 +16,14 @@ const SuccessOauthHandler = (req: Request, res: Response) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
     });
 
