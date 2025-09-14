775708e43e5baac19a5f8996d67411b722a95293
cr4ck-j4ck <vermapratyush486@gmail.com>
2025-09-14 09:41:34 +0530

    chore: add type-check script for CI and local validation
    - Adds "type-check": "tsc -b --noEmit" for running TS checks without emitting files

---
+++ Frontend/src/@Types/vite-env.d.ts
@@ -0,0 +1,12 @@
+interface ImportMetaEnv {
+  readonly VITE_BACKEND_API: string;
+    readonly VITE_FRONTEND_URL: string;
+    readonly VITE_LISTING_API: string;
+    readonly VITE_MAPTILER_KEY: string;
+    readonly VITE_BACKEND_API: string;
+  // Add other env variables here
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
